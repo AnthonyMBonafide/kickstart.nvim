d238a63cf483ca7739c3f2f0222316746971c4d8
Anthony M. Bonafide <AnthonyMBonafide@gmail.com>
2024-01-06 11:37:07 -0500

    Start to moving configs to separate files
    Signed-off-by: Anthony M. Bonafide <AnthonyMBonafide@gmail.com>

--- init.lua
+++ init.lua
@@ -43,42 +43,7 @@ P.S. You can delete this when you're done too. It's your config now :)
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-
-vim.cmd([[set guicursor=n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50
-  \,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor
-  \,sm:block-blinkwait175-blinkoff150-blinkon175]])
---vim.cmd('set guicursor=a:blinkon100')
-vim.keymap.set("n", "<C-Up>", ":resize -2<CR>")
-vim.keymap.set("n", "<C-Down>", ":resize +2<CR>")
-vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>")
-vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>")
-vim.opt.nu = true
-vim.opt.relativenumber = true
-
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-
-vim.opt.smartindent = true
-
-vim.opt.wrap = false
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
-
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-
-vim.opt.termguicolors = true
-
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
-
-vim.opt.updatetime = 50
-vim.opt.colorcolumn = "80"
+require('settings')
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    https://github.com/folke/lazy.nvim
---
+++ lua/keymappings.lua
@@ -0,0 +1,61 @@
+-- Navigation
+vim.keymap.set('i', 'jk', '<Esc>', { noremap = true })
+vim.keymap.set('n', 'J', '5j', { noremap = true })
+vim.keymap.set('n', 'K', '5k', { noremap = true })
+vim.keymap.set({ 'n' }, '<leader>S', ':w<cr>', { noremap = true, desc = 'Save File' })
+
+-- Pane navigation
+vim.keymap.set('n', '<C-l>', '<C-w>l>', { noremap = true })
+vim.keymap.set('n', '<C-h>', '<C-w>h>', { noremap = true })
+vim.keymap.set('n', '<C-j>', '<C-w>j>', { noremap = true })
+vim.keymap.set('n', '<C-k>', '<C-w>k>', { noremap = true })
+
+-- TODO map leader q to quit file quickly
+-- TODO map something to quickly open up directly listing(file explorer)
+--
+vim.keymap.set("n", "<leader>dl", vim.cmd.Ex, { desc = "Open Directory Listing NetRW" })
+
+-- Move higlighted text up and down and also formatting as text is moved, like
+-- in an if statement
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move highlighted text up" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move highlighted text down" })
+
+-- Enhances regular NVIM J which appends the line after the current one to the
+-- current line. However, this keeps the cursor at the front of the line rather
+-- continously moving to the end of the line during sucessive uses.
+vim.keymap.set("n", "<leader>j", "mzJ`z", { desc = "Append next line to current line" })
+-- Half page jumping like usual but keep cursor in middle of screen for less
+-- disorientation
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- Keep search target in the middle of the screen, less disorienting when doing
+-- this a lot quickly
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Update to P but this keeps the original text that was pasted in the register
+-- rather than the text that was removed being put into the register.
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = 'Paste without replacing register' })
+
+-- Yank text into OS clipboard which can be used outside of NVIM
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = 'yank into OS clipboard' })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = 'Yank into OS clipboard' })
+
+-- Delete without adding to paste register
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = 'Delete without adding to register' })
+
+-- Switch between projects
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>",
+  { desc = 'Switch between TMUX sessions' })
+-- Format code
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = 'Format code' })
+
+-- TODO fix so they do not collide with pane navigation.
+-- Quick fixes for errors
+--vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+--vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+--vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+--vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- Replace the word that is under your cursor, start typing right away
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
