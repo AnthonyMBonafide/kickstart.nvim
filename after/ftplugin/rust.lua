3193a4a99e95caa5903e790c42ffe75b85fa61bb
Anthony M. Bonafide <AnthonyMBonafide@gmail.com>
2024-01-08 10:47:05 -0500

    ading autoscroll
    Signed-off-by: Anthony M. Bonafide <AnthonyMBonafide@gmail.com>

---
+++ after/ftplugin/rust.lua
@@ -0,0 +1,1 @@
+-- vim.keymap.set('n', '<leader>cdb', vim.cmd.RustLsp('debuggables', 'last' --[[ optional ]]))
--- lua/lazyplugins.lua
+++ lua/lazyplugins.lua
@@ -23,6 +23,8 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: First, some plugins that don't require any configuration
 
+  -- Navigation
+  "karb94/neoscroll.nvim",
   -- Git related plugins
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
